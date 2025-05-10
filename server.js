00252e8face484657a563b7a5de8337a15ea4efa
Aswin <aswinpash666@gmail.com>
2025-05-10 15:16:08 +0530

    changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const otpStore = {}; // For OTP storage
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // Your frontend URL
+  origin: 'https://estore-frontend-x1sy.vercel.app', // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
