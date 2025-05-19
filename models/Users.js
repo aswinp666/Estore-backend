bf8b18b49b386cd04433d2d04a759605f559face
Aswin <aswinpash666@gmail.com>
2025-05-19 07:47:58 +0530

    Add cart management routes: implement endpoints for retrieving and updating user's cart items

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
+const cartItemSchema = new mongoose.Schema({
+  _id: { type: Number, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  discountedPrice: { type: Number },
+  quantity: { type: Number, required: true },
+  imageUrl: { type: String }
+});
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  cartItems: [cartItemSchema], // Add cart items to user schema
   createdAt: { type: Date, default: Date.now },
 });
 
