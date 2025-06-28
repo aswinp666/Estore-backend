0bc021250611589e3c488043948f062817da66b8
Aswin <aswinpash666@gmail.com>
2025-06-28 11:01:52 +0530

    Add addresses field to user schema for storing multiple address details

--- models/Users.js
+++ models/Users.js
@@ -5,6 +5,19 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+  addresses: [
+    {
+      firstName: String,
+      lastName: String,
+      companyName: String,
+      country: String,
+      address: String,
+      addressTwo: String,
+      town: String,
+      phone: String,
+      email: String,
+    },
+  ],
 });
 
 module.exports = mongoose.model("User", userSchema);
