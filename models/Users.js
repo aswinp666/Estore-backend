e103f67de45e2a89a820de454627235505d14344
Aswin <aswinpash666@gmail.com>
2025-06-28 11:09:45 +0530

    Remove addresses field from user schema to simplify user model

--- models/Users.js
+++ models/Users.js
@@ -5,19 +5,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  addresses: [
-    {
-      firstName: String,
-      lastName: String,
-      companyName: String,
-      country: String,
-      address: String,
-      addressTwo: String,
-      town: String,
-      phone: String,
-      email: String,
-    },
-  ],
 });
 
 module.exports = mongoose.model("User", userSchema);
